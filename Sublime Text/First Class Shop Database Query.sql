29f71991e061c47f9bb84bbbfa5c7c45c1fa568c
nsyawali12 <nsyawali12@gmail.com>
2017-11-20 23:30:47 +0700

    Penambahan PK

--- Sublime Text/First Class Shop Database Query.sql
+++ Sublime Text/First Class Shop Database Query.sql
@@ -26,7 +26,7 @@ create table Manager(
 );
 
 create table PenganggungJawab_BagianBarang(
-	Id_PjBarang varchar2(10),
+	Id_PjBarang varchar2(10), ----- perbaiki pk nya --
 	Nama_PjBarang varchar2(20),
 	Nomer_Telepon varchar2(15)
 );
@@ -38,25 +38,25 @@ create table Barang(
 );
 
 create table Kasir(
-	Id_Kasir varchar2(10),
+	Id_Kasir varchar2(10), --- perbaiki pknya ---
 	Nama_Kasir varchar2(20),
 	Nomer_Telepon varchar2(15)
 );
 
 create table Security(
-	Id_Security varchar2(10),
+	Id_Security varchar2(10), --- perbaiki pk nya ---
 	Nama_Security varchar2(20),
 	Nomer_Telepon varchar2(15)
 );
 
 create table montir(
-	Id_Montir varchar2(10),
+	Id_Montir varchar2(10), --- perbaiki pk nya ----
 	Nama_Montir varchar2(20),
 	Nomer_Telepon varchar2(15)
 );
 
 create table Alat(
-	Id_Alat varchar2 (10),
+	Id_Alat varchar2 (10), --- perbaiki pk ----- 
 	Nama_Alat varchar2(10),
 	Jenis_Alat varchar2(10),
 	Waktu DATE
@@ -97,7 +97,7 @@ create table R_Memiliki(
 );
 
 create table Bertanggung_Jawab(
-	Id_PJBarang varchar2(10),
+	Id_PJBarangR varchar2(10),
 	Id_Barang varchar2(10)
 );
 
@@ -105,3 +105,36 @@ create table R_Menggunakan(
 	Id_Alat varchar2(10),
 	Id_Montir varchar2(10)
 );
+
+--- ganti menjadi pegawai ----
+alter table PenganggungJawab_BagianBarang Rename Column Id_PJBarang to Id_Pegawai;
+alter table Kasir Rename Column Id_Kasir to Id_Pegawai;
+alter table Security Rename Column Id_Security to Id_Pegawai;
+alter table montir Rename Column Id_Montir to Id_Pegawai;
+
+------ penambahan ID_divisi pada anak anaknya
+alter table pegawai
+add column ID_divisi varchar2(10);
+
+-------- Penambahan PK pada Table Master
+alter table Pelanggan add constraint pk_Pelanggan 
+Primary key (Nomer_Identitas);
+
+alter table Shift add constraint pk_Shift
+Primary key (Kode_Shift);
+
+alter table Pelayanan add constraint pk_Pelayanan
+Primary key (Id_Pelayan);
+
+alter table Manager add constraint pk_Manager
+Primary key (Id_Manager);
+
+alter table Barang add constraint pk_Barang
+Primary key (Id_Barang);
+
+alter table Alat add constraint pk_Alat
+Primary key (Id_Alat);
+
+----- penambahan FK & PK pada table yang memilikinya
+
+
