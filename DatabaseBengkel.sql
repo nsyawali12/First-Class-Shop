029e9c0734a0b0fa4a5dceec1b78972a00ea017f
nsyawali12 <nsyawali12@gmail.com>
2017-11-22 01:30:06 +0700

    FK masih ada yang Error

--- DatabaseBengkel.sql
+++ DatabaseBengkel.sql
@@ -103,3 +103,101 @@ create table R_Menggunakan(
 	Id_Alat varchar2(10),
 	Id_Montir varchar2(10)
 );
+
+---- ganti menjadi pegawai
+alter table PenganggungJawab_BagianBarang Rename Column Id_PJBarang to Id_Pegawai;
+alter table Kasir Rename Column Id_Kasir to Id_Pegawai;
+alter table Security Rename Column Id_Security to Id_Pegawai;
+alter table montir Rename Column Id_Montir to Id_Pegawai;
+
+---- Penambahan Id_divisi pada anak - anaknya
+alter table Pegawai 
+add ID_divisi varchar2(10);
+
+alter table Shift add constraint pk_Shift
+Primary key (Kode_Shift);
+
+alter table Pelayanan add constraint pk_Pelayanan
+Primary key (Id_Pelayan);
+
+alter table Manager add constraint pk_Manager
+Primary key (Id_Manager);
+
+alter table Barang add constraint pk_Barang
+Primary key (Id_Barang);
+
+alter table Alat add constraint pk_Alat
+Primary key (Id_Alat);
+
+---- Penambahan PK pada table Master
+alter table Pelanggan 
+add constraint pk_Pelanggan Primary key (Nomer_Identitas);
+
+alter table Shift add constraint pk_Shift
+Primary key (Kode_Shift);
+
+alter table Pelayanan add constraint pk_Pelayanan
+Primary key (Id_Pelayan);
+
+alter table Manager add constraint pk_Manager
+Primary key (Id_Manager);
+
+alter table Barang add constraint pk_Barang
+Primary key (Id_Barang);
+
+alter table Alat add constraint pk_Alat
+Primary key (Id_Alat);
+
+---- Penambahan FK & PK pada table yang memilikinya
+
+Alter table Kendaraan
+Add constraint pk_Kendaraan Primary key (Nomer_Plat)
+Add constraint fk_Kendaraan Foreign key (Nomer_Identitas)
+references Pelanggan (Nomer_Identitas);
+
+Alter table Membership
+Add constraint pk_Membership Primary key (Id_Member)
+add constraint fk_Membership Foreign key (Nomer_Identitas)
+references Pelanggan (Nomer_Identitas);
+
+Alter table Pegawai
+add constraint pk_Pegawai Primary key (Id_Pegawai, Id_divisi)
+
+Alter table Pegawai
+add constraint fk_Pegawai Foreign key (Id_Manager)
+references Manager (Id_Manager);
+
+alter table Security
+add constraint pk_Security Primary key (Id_Pegawai, Id_divisi)
+add constraint fk_Security Foreign key (Id_Pegawai, Id_divisi)
+references pegawai (Id_Pegawai, Id_divisi);
+
+Alter table Montir
+add constraint pk_Montir Primary key (Id_Pegawai, Id_divisi)
+add constraint fk_Montir Primary key (Id_Pegawai, Id_divisi)
+references Pegawai (Id_Pegawai, Id_divisi);
+
+alter table Kasir
+add constraint pk_Kasir Primary key (Id_Pegawai, Id_divisi)
+add constraint fk_Kasir Foreign key (Id_Pegawai, Id_divisi)\
+references Pegawai (Id_Pegawai, Id_divisi);
+
+alter table PenanggungJawab_BagianBarang
+add constraint pk_PenanggungJawab_BagianBarang Primary key (Id_Pegawai, Id_divisi)
+add constraint fk_PenanggungJawab_BagianBarang Foreign key (Id_Pegawai, Id_divisi)
+references Pegawai (Id_Pegawai, Id_divisi);
+
+alter table R_Memiliki
+add constraint pk_R_Memiliki Primary key (Id_Pegawai, Kode_Shift)
+add constraint fk_R_Memiliki Foreign key (Id_Pegawai, Kode_Shift)
+references Pegawai, Shift (Id_Pegawai, Kode_Shift);
+
+alter table Bertanggung_Jawab
+add constraint pk_R_BertanggungJawab Primary key (Id_Pegawai, Id_Barang)
+add constraint fk_R_BertanggungJawab Foreign key (Id_Pegawai, Id_Barang)
+references PenanggungJawab_BagianBarang, Barang (Id_Pegawai, Id_Barang);
+
+alter table R_Menggunakan
+add constraint pk_R_Menggunakan Primary key (Id_alat, Id_Pegawai)
+add constraint fk_R_Menggunakan Foreign key (Id_Alat, Id_Pegawai)
+references Alat, Montir (Id_Alat, Id_Montir);
--- Sublime Text/First Class Shop Database Query.sql
+++ Sublime Text/First Class Shop Database Query.sql
@@ -14,7 +14,7 @@ create table Shift(
 );
 
 create table pelayanan(
-	Id_Pelayan varchar2(10),
+	Id_Pelayanan varchar2(10),
 	Jenis_Pelayanan varchar2(10),
 	Total_Harga number
 );
@@ -25,7 +25,7 @@ create table Manager(
 	Nomer_Telepon varchar2(15)
 );
 
-create table PenganggungJawab_BagianBarang(
+create table PenanggungJawab_BagianBarang(
 	Id_PjBarang varchar2(10), ----- perbaiki pk nya --
 	Nama_PjBarang varchar2(20),
 	Nomer_Telepon varchar2(15)
@@ -75,7 +75,7 @@ create table Membership(
 );
 
 create table Detil_Pelayanan(
-	Id_Pelayan varchar2(10),
+	Id_Pelayanan varchar2(10),
 	Id_Pegawai varchar2(10),
 	Nomer_Identitas varchar2(20)
 );
@@ -84,11 +84,8 @@ create table Pegawai(
 	Id_Pegawai varchar2(10),
 	Nama_Pegawai varchar2(10),
 	Nomer_Telepon varchar2(10),
-	Id_Montir varchar2(10),
-	Id_Kasir varchar2(10),
-	Id_Security varchar2(10),
-	Id_PjBarang varchar2(10),
-	Id_Manager varchar2(10)
+	Id_Manager varchar2(10),
+	Id_Divisi varchar2(10),
 );
 
 create table R_Memiliki(
@@ -97,24 +94,53 @@ create table R_Memiliki(
 );
 
 create table Bertanggung_Jawab(
-	Id_PJBarangR varchar2(10),
+	Id_Pegawai varchar2(10),
 	Id_Barang varchar2(10)
 );
 
 create table R_Menggunakan(
 	Id_Alat varchar2(10),
-	Id_Montir varchar2(10)
+	Id_Pegawai varchar2(10)
 );
 
 --- ganti menjadi pegawai ----
-alter table PenganggungJawab_BagianBarang Rename Column Id_PJBarang to Id_Pegawai;
+alter table PenanggungJawab_BagianBarang Rename Column Id_PJBarang to Id_Pegawai;
 alter table Kasir Rename Column Id_Kasir to Id_Pegawai;
 alter table Security Rename Column Id_Security to Id_Pegawai;
 alter table montir Rename Column Id_Montir to Id_Pegawai;
 
------- penambahan ID_divisi pada anak anaknya
+----- Penambahan Id pegawai pada Table Manager
+
+Alter Table Manager add column Id_Pegawai varchar2(10);
+
+------ penambahan ID_divisi pada anak anaknya dan juga bapaknya
 alter table pegawai
-add column ID_divisi varchar2(10);
+add ID_divisi varchar2(10);
+
+alter table montir
+add Id_Divisi varchar2(10);
+
+alter table Security
+add Id_Divisi varchar2(10);
+
+alter table Kasir
+add Id_Divisi varchar2(10);
+
+alter table PenanggungJawab_BagianBarang
+add Id_Divisi varchar2(10);
+
+----- asalnya pelayan jadi pelayanan karena sebelumnya typo
+alter table pelayanan
+rename column Id_Pelayan to Id_Pelayanan;
+
+--------- Penghapusan PK pada table pegawai selain ID_Pegawai
+
+alter table Pegawai drop Id_Montir;
+alter table Pegawai drop id_Kasir;
+alter table Pegawai drop Id_Security;
+alter table pegawai drop Id_PjBarang;
+
+
 
 -------- Penambahan PK pada Table Master
 alter table Pelanggan add constraint pk_Pelanggan 
@@ -137,4 +163,68 @@ Primary key (Id_Alat);
 
 ----- penambahan FK & PK pada table yang memilikinya
 
+Alter table Kendaraan
+Add constraint pk_Kendaraan Primary key (Nomer_Plat)
+Add constraint fk_Kendaraan Foreign key (Nomer_Identitas)
+references Pelanggan (Nomer_Identitas);
 
+Alter table Membership
+Add constraint pk_Membership Primary key (Id_Member)
+add constraint fk_Membership Foreign key (Nomer_Identitas)
+references Pelanggan (Nomer_Identitas);
+
+
+alter table Pegawai
+add constraint pk_Pegawai Primary key (Id_Pegawai, Id_divisi);
+
+
+Alter table Pegawai
+add constraint fk_Pegawai Foreign key (Id_Manager)
+references Manager (Id_Manager);
+
+-------- belum bisa
+alter table Detil_Pelayanan
+add constraint fk_Detil_Pelayanan Foreign key (Id_Pegawai, Id_Pelayanan, Nomer_Identitas)
+references pegawai, pelayanan, Pelanggan(Id_Pegawai, Id_Pelayanan, Nomer_Identitas);
+---- Belum bisa 
+
+alter table Security
+add constraint pk_Security Primary key (Id_Pegawai, Id_divisi)
+add constraint fk_Security Foreign key (Id_Pegawai, Id_divisi)
+references pegawai (Id_Pegawai, Id_divisi);
+--------- belum bisa
+alter table montir
+add constraint pk_Montir Primary key (Id_Pegawai, Id_divisi)
+add constraint fk_Montir Primary key (Id_Pegawai, Id_divisi)
+references Pegawai (Id_Pegawai, Id_divisi);
+
+alter table Kasir
+add constraint pk_Kasir Primary key (Id_Pegawai, Id_divisi)
+add constraint fk_Kasir Foreign key (Id_Pegawai, Id_divisi)\
+references Pegawai (Id_Pegawai, Id_divisi);
+
+alter table PenanggungJawab_BagianBarang
+add constraint pk_PenanggungJawab_BagianBarang Primary key (Id_Pegawai, Id_divisi)
+add constraint fk_PenanggungJawab_BagianBarang Foreign key (Id_Pegawai, Id_divisi)
+references Pegawai (Id_Pegawai, Id_divisi);
+
+alter table R_Memiliki
+add constraint pk_R_Memiliki Primary key (Id_Pegawai, Kode_Shift)
+add constraint fk_R_Memiliki Foreign key (Id_Pegawai, Kode_Shift)
+references Pegawai, Shift (Id_Pegawai, Kode_Shift);
+
+alter table Bertanggung_Jawab
+add constraint pk_R_BertanggungJawab Primary key (Id_Pegawai, Id_Barang)
+add constraint fk_R_BertanggungJawab Foreign key (Id_Pegawai, Id_Barang)
+references PenanggungJawab_BagianBarang, Barang (Id_Pegawai, Id_Barang);
+
+alter table R_Menggunakan
+add constraint pk_R_Menggunakan Primary key (Id_alat, Id_Pegawai)
+add constraint fk_R_Menggunakan Foreign key (Id_Alat, Id_Pegawai)
+references Alat, Montir (Id_Alat, Id_Montir);
+----belum bisa
+
+
+----- lanjutin lagi bsk
+
+--- silahkan coding dibawah ini
